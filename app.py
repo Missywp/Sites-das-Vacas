a5c04574a9e10bf547059b15c3b017916e23e89e
Fernanda Moraes <85085303+Fer9211@users.noreply.github.com>
2025-06-03 08:05:17 -0300

    Atualização

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from controllers.app_controller import create_app
 from users import user
 from configAmbiente import configAmbientes
 from dadosAtuais import dadosAtuais
+from historico import historico
 
 if __name__ == '__main__':
     app = create_app()
     app.register_blueprint(user, url_prefix='/')
     app.register_blueprint(configAmbientes, url_prefix='/')
     app.register_blueprint(dadosAtuais, url_prefix='/')
+    app.register_blueprint(historico, url_prefix='/')
     app.run(host='0.0.0.0', port=8080, debug=True)
