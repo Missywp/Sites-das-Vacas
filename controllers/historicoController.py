d383c0fcb7d87d52592c02d160d3acd205ebbb33
Melissa Weiss <melissaperussulo22@gmail.com>
2025-06-14 18:57:23 -0300

    Update historicoController.py

--- controllers/historicoController.py
+++ controllers/historicoController.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, render_template, redirect, url_for
-
+from utils.decorators import role_required
 historico = Blueprint("historico_blueprint", __name__, template_folder="templates")
 
 @historico.route('/historicos')
+@role_required(roles=['admin', 'veterinario','funcionario'])
 def historicos():
     return render_template('historico.html')
