076aef0426bec74628f17cbda49c2c4a3c0b5d2f
Melissa Weiss <melissaperussulo22@gmail.com>
2025-06-14 19:02:19 -0300

    Create decorators.py

---
+++ decorators.py
@@ -0,0 +1,18 @@
+from flask import session, redirect, url_for, flash
+from functools import wraps
+
+def role_required(roles):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if 'user_id' not in session:
+                flash('É necessário fazer login para acessar esta página.', 'warning')
+                return redirect(url_for('user_blueprint.validated_user'))
+
+            if session.get('role') not in roles:
+                flash('Você não tem permissão para acessar esta página.', 'danger')
+                return redirect(url_for('user_blueprint.listarUser'))
+
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
