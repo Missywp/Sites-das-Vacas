8a37daf1dc74384cc44d9d98262b3aa4818f6270
Melissa Weiss <melissaperussulo22@gmail.com>
2025-06-14 18:56:36 -0300

    Update configAmbienteController.py

--- controllers/configAmbienteController.py
+++ controllers/configAmbienteController.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, render_template, redirect, url_for
+from utils.decorators import role_required
 
 configAmbientes = Blueprint("configAmbiente_blueprint", __name__, template_folder="templates")
 
 @configAmbientes.route('/configAmbiente')
+@role_required(roles=['admin', 'veterinario'])
 def configAmbiente():
     return render_template('configAmbiente.html')
