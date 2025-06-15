4352d40509c543974ac052fe96e8c42bdf8989a0
Melissa Weiss <melissaperussulo22@gmail.com>
2025-06-15 01:40:13 -0300

    Update dadosAtuaisController.py

--- controllers/dadosAtuaisController.py
+++ controllers/dadosAtuaisController.py
@@ -3,6 +3,6 @@ from utils.decorators import role_required
 dadosAtuais = Blueprint("dadosAtuais_blueprint", __name__, template_folder="templates")
 
 @dadosAtuais.route('/dadoAtual')
-@role_required(roles=['admin', 'funcionario, veterinario'])
+@role_required(roles=['admin', 'funcionario', 'veterinario'])
 def dadoAtual():
     return render_template('dadosAtuais.html')
