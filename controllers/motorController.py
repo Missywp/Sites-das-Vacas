d0b5d3ed84e34e05a72136a310bedad6dfe48d09
Melissa Weiss <melissaperussulo22@gmail.com>
2025-06-15 01:41:17 -0300

    Update motorController.py

--- controllers/motorController.py
+++ controllers/motorController.py
@@ -3,16 +3,16 @@ from utils.decorators import role_required
 motor = Blueprint("motor_blueprint", __name__, template_folder="templates")
 
 @motor.route('/motores')
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin', 'veterinario','funcionario'])
 def motores():
     return render_template("motor.html")
 
 @motor.route('/cadastrarMotor')
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin'])
 def cadastrarMotor():
     return render_template('cadastrarMotor.html')
 
 @motor.route("editarMotor")
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin'])
 def editarMotor():
     return render_template("editarMotor.html")
