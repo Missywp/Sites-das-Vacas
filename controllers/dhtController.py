76ca78ea26736bf9507096ef189c99c3b54b7e68
Melissa Weiss <melissaperussulo22@gmail.com>
2025-06-15 01:40:29 -0300

    Update dhtController.py

--- controllers/dhtController.py
+++ controllers/dhtController.py
@@ -4,16 +4,16 @@ from utils.decorators import role_required
 dht = Blueprint("dht_blueprint", __name__, template_folder="templates")
 
 @dht.route("/dhts")
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin', 'veterinario', 'funcionario'])
 def dhts():
     return render_template("dht.html")
 
 @dht.route('/cadastrarDht')
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin'])
 def cadastrarDht():
     return render_template('cadastrarDHT.html')
 
 @dht.route("/edtiarDht")
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin'])
 def editarDht():
     return render_template("editarDht.html")
