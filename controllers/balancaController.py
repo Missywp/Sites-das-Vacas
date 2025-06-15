a124fba5496ff2a0d9dc10a15eef953d5b5b146c
Melissa Weiss <melissaperussulo22@gmail.com>
2025-06-15 01:39:33 -0300

    Update balancaController.py

--- controllers/balancaController.py
+++ controllers/balancaController.py
@@ -4,16 +4,16 @@ from utils.decorators import role_required
 balanca = Blueprint("balanca_blueprint", __name__, template_folder="templates")
 
 @balanca.route('/balancas')
-@role_required(roles=['admin', 'veterinario' 'funcionario'])
+@role_required(roles=['admin', 'funcionario', 'veterinario'])
 def balancas():
     return render_template('balanca.html')
 
 @balanca.route('/cadastrarBalanca')
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin'])
 def cadastrarBalanca():
     return render_template('cadastrarBalanca.html')
 
 @balanca.route('/editarBalanca')
-@role_required(roles=['admin', 'veterinario'])
+@role_required(roles=['admin'])
 def editarBalanca():
     return render_template('editarBalanca.html')
